e0d7e721506b09675cc2c6d6877ad90205e60e0a
MinhHieu <lyminhhieudlf1@gmail.com>
2023-04-12 02:52:00 +0700

    Thêm API getSingle Product

--- backend/controllers/productcontroller.js
+++ backend/controllers/productcontroller.js
@@ -134,3 +134,33 @@ exports.getSingleProduct = catchAsyncErrors(async (req, res, next) => {
     })
 
 })
+// Get all products   =>   /api/v1/products?keyword=apple
+exports.getProducts = catchAsyncErrors(async (req, res, next) => {
+
+    const resPerPage = 12;
+    const productsCount = await Product.countDocuments();
+
+    const apiFeatures = new APIFeatures(Product.find(), req.query)
+        .search()
+        .filter()
+
+    let products = await apiFeatures.query;
+    let filteredProductsCount = products.length;
+
+    apiFeatures.pagination(resPerPage)
+    products = await apiFeatures.query;
+
+
+    setTimeout(function () {
+        res.status(200).json({
+            success: true,
+            productsCount,
+            resPerPage,
+            filteredProductsCount,
+            products
+        })
+    }, 2000)
+
+
+})
+
